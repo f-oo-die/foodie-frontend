e9720ec5ae0071cc44d9cf0e38b940aa6232ab44
Dzenita Djulovic <djulovic.dzenita1999@gmail.com>
2021-04-14 20:45:06 +0200

    modified routing and added http client module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './components/home/home.component';
+import { RecipesComponent } from './components/recipes/recipes.component';
 
 const routes: Routes = [
-  { path: 'home', component:HomeComponent }
+  { path: '', component:HomeComponent },
+  { path: 'recipes', component:RecipesComponent }
 ];
 
 @NgModule({
