6ea2d1c21940aa63569a008fd5f32336a62b1aa3
Armin Salihovic <armin.salihovic@live.com>
2021-05-11 18:38:58 +0200

    fix tslint whitespace

--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -8,7 +8,7 @@ import {environment} from '../../environments/environment';
   providedIn: 'root'
 })
 export class RecipeService {
-  apiUrl:string = environment.api;
+  apiUrl: string = environment.api;
 
   constructor(private http: HttpClient) { }
 
