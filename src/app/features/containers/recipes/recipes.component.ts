be742c787061056f8aeee4bc54dac91329cc7130
Dzenita Djulovic <djulovic.dzenita1999@gmail.com>
2021-05-22 01:33:49 +0200

    added search by recipe title function

--- src/app/features/containers/recipes/recipes.component.ts
+++ src/app/features/containers/recipes/recipes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Recipe } from 'src/app/interface/recipe';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-recipes',
@@ -15,10 +16,23 @@ export class RecipesComponent implements OnInit {
     ) {}
 
   ngOnInit(): void {
-    this.activatedRoute.data.subscribe(this.loadRecipes);
+    this.getRecipes();
   }
 
-  loadRecipes = (routeData: { recipes: Recipe[] }) => {
-    this.recipes = routeData.recipes;
+  public getRecipes(): void {
+    this.activatedRoute.data.subscribe((routeData: { recipes: Recipe[] }) => {
+      this.recipes = routeData.recipes;
+    });
+  }
+
+  public searchRecipes(key: String): void {
+    const result: Recipe[] = [];
+    for (const recipe of this.recipes){
+      if (recipe.title.toLowerCase().indexOf(key.toLowerCase()) !== -1){
+        result.push(recipe);
+      }
+    }
+    this.recipes = result;
+    if (result.length === 0 || !key) this.getRecipes();
   }
 }
