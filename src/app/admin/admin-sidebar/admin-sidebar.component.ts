2cba2a0d191dfe1034aa976e6fde3b58b0e46551
Armin Salihovic <armin.salihovic@live.com>
2021-05-25 05:58:32 +0200

    separate sidebar into sidebar component

---
+++ src/app/admin/admin-sidebar/admin-sidebar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-sidebar',
+  templateUrl: './admin-sidebar.component.html',
+  styleUrls: ['./admin-sidebar.component.css', '../../../assets/css/admin.css']
+})
+export class AdminSidebarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
