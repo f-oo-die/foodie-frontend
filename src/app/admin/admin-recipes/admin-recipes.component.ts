324f218db8eff4e17ae65b7a71f32f048920fe84
Armin Salihovic <armin.salihovic@live.com>
2021-05-25 15:21:04 +0200

    add admin recipes datatable

--- src/app/admin/admin-recipes/admin-recipes.component.ts
+++ src/app/admin/admin-recipes/admin-recipes.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../../interface/recipe';
+import {ActivatedRoute, Router} from '@angular/router';
+import {RoutesConstant} from '../../constants/routes-constant';
 
 @Component({
   selector: 'app-admin-recipes',
@@ -6,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-recipes.component.css', '../../../assets/css/admin.css']
 })
 export class AdminRecipesComponent implements OnInit {
+  recipes: Recipe[];
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
+    this.activatedRoute.data.subscribe((routeData: { recipes: Recipe[] }) => {
+      this.recipes = routeData.recipes;
+    });
+  }
+
+  openAddRecipe(): void {
+    this.router.navigateByUrl(`/${RoutesConstant.ADMIN_RECIPES_NEW}`);
   }
 
 }
