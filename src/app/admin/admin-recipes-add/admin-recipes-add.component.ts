c12805d3824a6cb97fe9ee09013dd685db991024
Armin Salihovic <armin.salihovic@live.com>
2021-05-25 15:09:20 +0200

    add admin recipes add component

---
+++ src/app/admin/admin-recipes-add/admin-recipes-add.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-recipes-add',
+  templateUrl: './admin-recipes-add.component.html',
+  styleUrls: ['./admin-recipes-add.component.css', '../../../assets/css/admin.css']
+})
+export class AdminRecipesAddComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
