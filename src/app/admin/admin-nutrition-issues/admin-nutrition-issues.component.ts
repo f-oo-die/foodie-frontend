c32eaf3e761af68b54b423e80042f40d24a83da0
Armin Salihovic <armin.salihovic@live.com>
2021-05-26 22:21:41 +0200

    add admin nutrition issue component

---
+++ src/app/admin/admin-nutrition-issues/admin-nutrition-issues.component.ts
@@ -0,0 +1,26 @@
+import {Component, OnInit} from '@angular/core';
+import {NutritionIssue} from '../../interface/nutrition-issue';
+import {ActivatedRoute, Router} from '@angular/router';
+import {RoutesConstant} from '../../constants/routes-constant';
+
+@Component({
+  selector: 'app-admin-nutrition-issues',
+  templateUrl: './admin-nutrition-issues.component.html',
+  styleUrls: ['./admin-nutrition-issues.component.css', '../../../assets/css/admin.css']
+})
+export class AdminNutritionIssuesComponent implements OnInit {
+
+  nutritionIssues: NutritionIssue[];
+
+  constructor(private activatedRoute: ActivatedRoute, private router: Router) { }
+
+  ngOnInit(): void {
+    this.activatedRoute.data.subscribe((routeData: { nutritionIssues: NutritionIssue[] }) => {
+      this.nutritionIssues = routeData.nutritionIssues;
+    });
+  }
+
+  openAddNutritionIssues(): void {
+    this.router.navigateByUrl(`/${RoutesConstant.ADMIN_NUTRITION_ISSUES_NEW}`);
+  }
+}
