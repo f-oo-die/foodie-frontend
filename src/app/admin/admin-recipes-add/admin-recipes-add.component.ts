38f40fd1d7983d27abf1ad678a2898e48de2a823
Armin Salihovic <armin.salihovic@live.com>
2021-05-26 23:24:39 +0200

    implement form functionality (works~ 50%)

--- src/app/admin/admin-recipes-add/admin-recipes-add.component.ts
+++ src/app/admin/admin-recipes-add/admin-recipes-add.component.ts
@@ -1,4 +1,9 @@
 import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {RecipeService} from '../../services/recipe.service';
+import {Recipe} from '../../interface/recipe';
+import {RoutesConstant} from '../../constants/routes-constant';
+import {TypeOfMeal} from '../../interface/enums/typeOfMeal';
 
 @Component({
   selector: 'app-admin-recipes-add',
@@ -7,9 +12,17 @@ import {Component, OnInit} from '@angular/core';
 })
 export class AdminRecipesAddComponent implements OnInit {
 
-  constructor() { }
+  recipeModel: Recipe = {numOfCalories: 0, preparation: '', title: '', typeOfMeal: 0};
+  typeOfMeal = TypeOfMeal;
+
+  constructor(private recipeService: RecipeService, private router: Router) { }
 
   ngOnInit(): void {
+  }
 
+  onSubmit(): void {
+    this.recipeService.createRecipe(this.recipeModel).subscribe(() => {
+      this.router.navigateByUrl(`/${RoutesConstant.ADMIN_RECIPES_LIST}`);
+    });
   }
 }
