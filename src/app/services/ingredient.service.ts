8e07e52421b30aad9049b60466e69a1bb436f639
Armin Salihovic <armin.salihovic@live.com>
2021-05-26 22:27:44 +0200

    add createIngredient method

--- src/app/services/ingredient.service.ts
+++ src/app/services/ingredient.service.ts
@@ -1,17 +1,22 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { Ingredient } from '../interface/ingredient';
+import {HttpClient} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
+import {Ingredient} from '../interface/ingredient';
+import {environment} from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class IngredientService {
-  apiUrl:string = 'http://localhost:8080';
+  apiUrl: string = environment.api;
 
   constructor(private http: HttpClient) { }
 
   getIngredients(): Observable<Ingredient[]>{
     return this.http.get<Ingredient[]>(`${this.apiUrl}/ingredients`);
+  }
+
+  createIngredient(ingredient: Ingredient): Observable<Ingredient> {
+    return this.http.post<Ingredient>(`${this.apiUrl}/ingredients`, ingredient);
   }
 }
