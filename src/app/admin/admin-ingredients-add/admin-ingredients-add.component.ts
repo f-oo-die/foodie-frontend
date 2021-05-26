d2aadc4599bf33d0c116ff14d74ea9c7aaa34a9a
Armin Salihovic <armin.salihovic@live.com>
2021-05-26 22:23:44 +0200

    add admin: ingredients add component

---
+++ src/app/admin/admin-ingredients-add/admin-ingredients-add.component.ts
@@ -0,0 +1,26 @@
+import {Component, OnInit} from '@angular/core';
+import {RoutesConstant} from '../../constants/routes-constant';
+import {Router} from '@angular/router';
+import {IngredientService} from '../../services/ingredient.service';
+import {Ingredient} from '../../interface/ingredient';
+
+@Component({
+  selector: 'app-admin-ingredients-add',
+  templateUrl: './admin-ingredients-add.component.html',
+  styleUrls: ['./admin-ingredients-add.component.css', '../../../assets/css/admin.css']
+})
+export class AdminIngredientsAddComponent implements OnInit {
+
+  ingredientModel: Ingredient = {caloricValue: 0, ingredientName: '', nutritionalValue: 0};
+
+  constructor(private ingredientsService: IngredientService, private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(): void {
+    this.ingredientsService.createIngredient(this.ingredientModel).subscribe(() => {
+      this.router.navigateByUrl(`/${RoutesConstant.ADMIN_INGREDIENTS_LIST}`);
+    });
+  }
+}
