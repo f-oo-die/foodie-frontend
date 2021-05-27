31115f58eccef6d870d569a43e2908b105f56ba7
Armin Salihovic <armin.salihovic@live.com>
2021-05-27 05:49:55 +0200

    implement form functionality (works~ 65%)

--- src/app/admin/admin-recipes-add/admin-recipes-add.component.ts
+++ src/app/admin/admin-recipes-add/admin-recipes-add.component.ts
@@ -1,9 +1,12 @@
 import {Component, OnInit} from '@angular/core';
-import {Router} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import {RecipeService} from '../../services/recipe.service';
 import {Recipe} from '../../interface/recipe';
 import {RoutesConstant} from '../../constants/routes-constant';
 import {TypeOfMeal} from '../../interface/enums/typeOfMeal';
+import {IngredientList} from '../../interface/ingredientList';
+import {Ingredient} from '../../interface/ingredient';
+import {NutritionIssue} from '../../interface/nutrition-issue';
 
 @Component({
   selector: 'app-admin-recipes-add',
@@ -11,13 +14,27 @@ import {TypeOfMeal} from '../../interface/enums/typeOfMeal';
   styleUrls: ['./admin-recipes-add.component.css', '../../../assets/css/admin.css']
 })
 export class AdminRecipesAddComponent implements OnInit {
+  // todo: find a way to implement adding recipes fully
 
-  recipeModel: Recipe = {numOfCalories: 0, preparation: '', title: '', typeOfMeal: 0};
+  ingredient1: Ingredient = {caloricValue: 1.0, id: 1, ingredientName: '2', nutritionalValue: 3.0};
+  ingredientList1: IngredientList = {amount: 192, amountLabel: 'XX', ingredient: this.ingredient1};
+
+  recipeModel: Recipe = {ingredientList: [this.ingredientList1], nutritionIssues: [], numOfCalories: 0, preparation: '', title: '', typeOfMeal: 0};
   typeOfMeal = TypeOfMeal;
 
-  constructor(private recipeService: RecipeService, private router: Router) { }
+  nutritionIssues: NutritionIssue[];
+  ingredients: Ingredient[];
+
+  constructor(private recipeService: RecipeService, private activatedRoute: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
+    this.activatedRoute.data.subscribe((routeData: { nutritionIssues: NutritionIssue[] }) => {
+      this.nutritionIssues = routeData.nutritionIssues;
+    });
+
+    this.activatedRoute.data.subscribe((routeData: { ingredients: Ingredient[] }) => {
+      this.ingredients = routeData.ingredients;
+    });
   }
 
   onSubmit(): void {
