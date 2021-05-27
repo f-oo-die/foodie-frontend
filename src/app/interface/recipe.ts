721c31a756c9038a23dd5e56e3b5d5494a4bf89e
Armin Salihovic <armin.salihovic@live.com>
2021-05-27 05:43:11 +0200

    add ingredientList & nutritionIssues to recipe interface

--- src/app/interface/recipe.ts
+++ src/app/interface/recipe.ts
@@ -1,7 +1,12 @@
+import {IngredientList} from './ingredientList';
+import {NutritionIssue} from './nutrition-issue';
+
 export interface Recipe {
   id?: number;
   title: string;
   preparation: string;
   numOfCalories: number;
   typeOfMeal: number;
+  ingredientList: IngredientList[];
+  nutritionIssues: NutritionIssue[];
 }
