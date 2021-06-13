824e10e628270d3633bbcd6773b30b7dd72cc0aa
Dzenita Djulovic <djulovic.dzenita1999@gmail.com>
2021-06-14 00:47:55 +0200

    added comment resolver and service

---
+++ src/app/resolvers/comments.resolver.ts
@@ -0,0 +1,15 @@
+import {ActivatedRouteSnapshot, Resolve, RouterStateSnapshot} from '@angular/router';
+import {Observable} from 'rxjs';
+import {Injectable} from '@angular/core';
+import { CommentService } from '../services/comment.service';
+import { Comment } from '../interface/comment';
+
+@Injectable()
+export class CommentsResolver implements Resolve<Comment[]> {
+
+  constructor(private commentService: CommentService) {}
+
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Comment[]> {
+    return this.commentService.getComments(route.paramMap.get('id'));
+  }
+}
---
+++ src/app/services/comment.service.ts
@@ -0,0 +1,22 @@
+import {HttpClient} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
+import {environment} from '../../environments/environment';
+import { Comment } from '../interface/comment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommentService {
+  apiUrl: string = environment.api;
+
+  constructor(private http: HttpClient) { }
+
+  getComments(recipeId: string): Observable<Comment[]>{
+    return this.http.get<Comment[]>(`${this.apiUrl}/comments/${recipeId}`);
+  }
+
+  createComment(recipeId: number, comment: Comment): Observable<Comment> {
+    return this.http.post<Comment>(`${this.apiUrl}/comments/${recipeId}`, comment);
+  }
+}
